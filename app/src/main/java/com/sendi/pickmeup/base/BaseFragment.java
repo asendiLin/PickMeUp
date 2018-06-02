fe6da5ee3d3ab167d6bdeb715f7bdbffb409bb9b
lizheng98 <gokoururi@icloud.com>
2018-06-02 10:09:59 +0800

    6-2 update

---
+++ app/src/main/java/com/sendi/pickmeup/base/BaseFragment.java
@@ -0,0 +1,68 @@
+package com.sendi.pickmeup.base;
+
+import android.arch.lifecycle.Lifecycle;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lizheng on 2018/6/2.
+ */
+
+public abstract class BaseFragment extends Fragment {
+        protected Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView(inflater,container);
+    }
+
+    public abstract View initView(LayoutInflater inflater,ViewGroup container);
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
